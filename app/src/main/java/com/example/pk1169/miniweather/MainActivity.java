936e30d5f46900f9408294f2580170d24c771517
pk1169 <1169222013@qq.com>
2017-09-25 21:15:40 +0800

    androidTest

---
+++ app/src/main/java/com/example/pk1169/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pk1169.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pk1169 on 2017/9/25.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
